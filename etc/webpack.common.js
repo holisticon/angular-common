1c11fc65a77854e2b0c2380466b8978e3d8f38e9
Martin Reinhardt <contact@martinreinhardt-online.de>
2017-01-19 16:12:52 +0100

    feat(logging): Improve logging
    see #20

--- etc/webpack.common.js
+++ etc/webpack.common.js
@@ -35,21 +35,47 @@ const METADATA = {
 var config = {
 
   stats: {
-    hash: false,
-    version: false,
-    timings: false,
-    assets: false,
-    chunks: false,
-    modules: false,
-    reasons: false,
-    children: false,
-    source: false,
+    // Add asset Information
+    assets: true,
+    // Sort assets by a field
+    assetsSort: "field",
+    // Add information about cached (not built) modules
+    cached: true,
+    // Add children information
+    children: true,
+    // Add chunk information (setting this to `false` allows for a less verbose output)
+    chunks: true,
+    // Add built modules information to chunk information
+    chunkModules: true,
+    // Add the origins of chunks and chunk merging info
+    chunkOrigins: true,
+    // Sort the chunks by a field
+    chunksSort: "field",
+    // Context directory for request shortening
+    context: "../src/",
+    // Add errors
     errors: true,
+    // Add details to errors (like resolving log)
     errorDetails: true,
-    warnings: false,
-    publicPath: false
+    // Add the hash of the compilation
+    hash: true,
+    // Add built modules information
+    modules: true,
+    // Sort the modules by a field
+    modulesSort: "field",
+    // Add public path information
+    publicPath: true,
+    // Add information about the reasons why modules are included
+    reasons: true,
+    // Add the source code of modules
+    source: true,
+    // Add timing information
+    timings: true,
+    // Add webpack version information
+    version: true,
+    // Add warnings
+    warnings: true
   },
-
   /*
    * The entry point for the bundle
    * Our Angular.js app
