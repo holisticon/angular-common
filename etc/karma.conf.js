2b22b91dc1ee50a5001b08ad0d9530ab72651235
Reinhardt, Martin <contact@martinreinhardt-online.de>
2017-11-02 16:36:04 +0100

    fix(phantom-timeout): Fix phantomjs timeout error

--- etc/karma.conf.js
+++ etc/karma.conf.js
@@ -18,152 +18,152 @@ const JUNIT = appConfig.junit;
 const specs = appConfig.testSpecs;
 const bundle = appConfig.testBundle;
 
-module.exports = function (config) {
+module.exports = function(config) {
 
-  config.set({
+    config.set({
 
-    // see https://github.com/karma-runner/karma-junit-reporter/issues/61
-    browserNoActivityTimeout: 30000,
+        // see https://github.com/karma-runner/karma-junit-reporter/issues/61
+        browserNoActivityTimeout: 120000,
 
-    // base path that will be used to resolve all patterns (e.g. files, exclude)
-    basePath: '',
+        // base path that will be used to resolve all patterns (e.g. files, exclude)
+        basePath: '',
 
-    // see https://github.com/angular/angular-cli/issues/2125
-    mime: {
-      'text/x-typescript': ['ts', 'tsx']
-    },
+        // see https://github.com/angular/angular-cli/issues/2125
+        mime: {
+            'text/x-typescript': ['ts', 'tsx']
+        },
 
-    /*
-     * Frameworks to use
-     *
-     * available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-     */
-    frameworks: ['jasmine'],
+        /*
+         * Frameworks to use
+         *
+         * available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+         */
+        frameworks: ['jasmine'],
 
-    // list of files to exclude
-    exclude: [],
+        // list of files to exclude
+        exclude: [],
 
-    /*
-     * list of files / patterns to load in the browser
-     *
-     * we are building the test environment in ./spec-bundle.js
-     */
-    files: [
-      bundle
-    ],
+        /*
+         * list of files / patterns to load in the browser
+         *
+         * we are building the test environment in ./spec-bundle.js
+         */
+        files: [
+            bundle
+        ],
 
-    /*
-     * preprocess matching files before serving them to the browser
-     * available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-     */
-    preprocessors: {
-      [bundle]: ['coverage', 'webpack', 'sourcemap']
-    },
+        /*
+         * preprocess matching files before serving them to the browser
+         * available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+         */
+        preprocessors: {
+            [bundle]: ['coverage', 'webpack', 'sourcemap']
+        },
 
-    webpack: webpackConfig,
+        webpack: webpackConfig,
 
 
-    coverageReporter: {
-      type: 'in-memory'
-    },
+        coverageReporter: {
+            type: 'in-memory'
+        },
 
-    coverageIstanbulReporter: {
-        reports: [ 'text-summary' ],
-        fixWebpackSourcePaths: true
-    },
+        coverageIstanbulReporter: {
+            reports: ['text-summary'],
+            fixWebpackSourcePaths: true
+        },
 
-    remapCoverageReporter: {
-      'text-summary': null,
-      json: 'target/ccoverage/coverage.json',
-      html: 'target/ccoverage/html'
-    },
+        remapCoverageReporter: {
+            'text-summary': null,
+            json: 'target/ccoverage/coverage.json',
+            html: 'target/ccoverage/html'
+        },
 
-    // Webpack please don't spam the console when running in karma!
-    //webpackServer: {noInfo: true},
+        // Webpack please don't spam the console when running in karma!
+        //webpackServer: {noInfo: true},
 
-    webpackMiddleware: {
-      stats: { // Add asset Information
-        assets: isDebug,
-        // Sort assets by a field
-        assetsSort: "field",
-        // Add information about cached (not built) modules
-        cached: isDebug,
-        // Add children information
-        children: isDebug,
-        // Add chunk information (setting this to `false` allows for a less verbose output)
-        chunks: isDebug,
-        // Add built modules information to chunk information
-        chunkModules: isDebug,
-        // Add the origins of chunks and chunk merging info
-        chunkOrigins: isDebug,
-        // Sort the chunks by a field
-        chunksSort: "field",
-        // Context directory for request shortening
-        context: appConfig.srcPath,
-        // Add errors
-        errors: true,
-        // Add details to errors (like resolving log)
-        errorDetails: true,
-        colors: true
-      }
-    },
-    /*
-     * test results reporter to use
-     *
-     * possible values: 'dots', 'progress'
-     * available reporters: https://npmjs.org/browse/keyword/karma-reporter
-     */
-    reporters: ['progress', 'junit', 'coverage', 'remap-coverage', 'kjhtml'],
+        webpackMiddleware: {
+            stats: { // Add asset Information
+                assets: isDebug,
+                // Sort assets by a field
+                assetsSort: "field",
+                // Add information about cached (not built) modules
+                cached: isDebug,
+                // Add children information
+                children: isDebug,
+                // Add chunk information (setting this to `false` allows for a less verbose output)
+                chunks: isDebug,
+                // Add built modules information to chunk information
+                chunkModules: isDebug,
+                // Add the origins of chunks and chunk merging info
+                chunkOrigins: isDebug,
+                // Sort the chunks by a field
+                chunksSort: "field",
+                // Context directory for request shortening
+                context: appConfig.srcPath,
+                // Add errors
+                errors: true,
+                // Add details to errors (like resolving log)
+                errorDetails: true,
+                colors: true
+            }
+        },
+        /*
+         * test results reporter to use
+         *
+         * possible values: 'dots', 'progress'
+         * available reporters: https://npmjs.org/browse/keyword/karma-reporter
+         */
+        reporters: ['progress', 'junit', 'coverage', 'remap-coverage', 'kjhtml'],
 
-    /**
-     * See https://github.com/karma-runner/karma-junit-reporter#configuration
-     */
-    junitReporter: JUNIT,
+        /**
+         * See https://github.com/karma-runner/karma-junit-reporter#configuration
+         */
+        junitReporter: JUNIT,
 
-    // web server port
-    port: 9876,
+        // web server port
+        port: 9876,
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-    /*
-     * level of logging
-     * possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-     */
-    logLevel: config.LOG_INFO,
+        /*
+         * level of logging
+         * possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+         */
+        logLevel: config.LOG_INFO,
 
-    // enable / disable watching file and executing tests whenever any file changes
+        // enable / disable watching file and executing tests whenever any file changes
 
-    autoWatch: true,
+        autoWatch: true,
 
-    /*
-     * custom launchers to run tests with
-     */
-    customLaunchers: {
-      Chrome_headless: {
-        base: 'Chrome',
-        flags: ['--headless', '--disable-gpu']
-      }
-    },
+        /*
+         * custom launchers to run tests with
+         */
+        customLaunchers: {
+            Chrome_headless: {
+                base: 'Chrome',
+                flags: ['--headless', '--disable-gpu']
+            }
+        },
 
-    /*
-     * start these browsers
-     * available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-     */
-    browsers: [
-      'PhantomJS',
-      //'Chrome_headless'
-    ],
+        /*
+         * start these browsers
+         * available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+         */
+        browsers: [
+            'PhantomJS',
+            //'Chrome_headless'
+        ],
 
-    /*
-     * Continuous Integration mode
-     * if true, Karma captures browsers, runs the tests and exits
-     */
-    singleRun: false
-  });
+        /*
+         * Continuous Integration mode
+         * if true, Karma captures browsers, runs the tests and exits
+         */
+        singleRun: false
+    });
 
-  config.files.push({ pattern: specs, watched: false });
-  config.preprocessors[specs] = ['webpack', 'sourcemap'];
+    config.files.push({ pattern: specs, watched: false });
+    config.preprocessors[specs] = ['webpack', 'sourcemap'];
 
-  debugLog('Using following karma config:', config);
+    debugLog('Using following karma config:', config);
 };
